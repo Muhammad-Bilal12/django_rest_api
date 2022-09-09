0b8a818ccd422f3c71f84d8d3be5a047b558b48d
Muhammad Bilal <muhammadbilalmnaeem@gmail.com>
2022-09-08 21:16:40 -0700

    api method

--- home/views.py
+++ home/views.py
@@ -3,9 +3,29 @@ from rest_framework.response import Response
 
 # Create your views here.
 
-@api_view()
+@api_view(['GET','POST','PATCH'])
 def home(request):
-    return Response({
-        'status': 200,
-        'message': 'Yes! Django rest framework start!',
+    if request.method == 'GET':
+        return Response({
+            'status': 200,
+            'message': 'Yes! Django rest framework start!',
+            'method' : 'Calling GET'
+    })
+    elif request.method == 'POST':
+        return Response({
+            'status': 200,
+            'message': 'Yes! Django rest framework start!',
+            'method' : 'Calling POST'
+    })
+    elif request.method == 'PATCH':
+        return Response({
+            'status': 200,
+            'message': 'Yes! Django rest framework start!',
+            'method' : 'Calling PATCH'
+    })
+    else :
+        return Response({
+            'status': 200,
+            'message': 'Yes! Django rest framework start!',
+            'method' : 'INVALID'
     })
